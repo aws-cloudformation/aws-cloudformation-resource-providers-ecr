e7ecefec26866b4dd0c67954c93b5fd21a3219b0
Brad Gray <72415618+bradgr@users.noreply.github.com>
2021-07-22 16:49:00 -0700

    add support for replicationConfiguration list handler (#36)

--- aws-ecr-replicationconfiguration/src/test/java/software/amazon/ecr/replicationconfiguration/ListHandlerTest.java
+++ aws-ecr-replicationconfiguration/src/test/java/software/amazon/ecr/replicationconfiguration/ListHandlerTest.java
@@ -1,13 +1,14 @@
 package software.amazon.ecr.replicationconfiguration;
 
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import software.amazon.awssdk.awscore.exception.AwsServiceException;
 import software.amazon.awssdk.services.ecr.EcrClient;
 import software.amazon.awssdk.services.ecr.model.DescribeRegistryRequest;
+import software.amazon.awssdk.services.ecr.model.ValidationException;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
 import software.amazon.cloudformation.proxy.HandlerErrorCode;
 import software.amazon.cloudformation.proxy.OperationStatus;
@@ -15,15 +16,12 @@ import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.ProxyClient;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class ListHandlerTest extends AbstractTestBase {
+class ListHandlerTest extends AbstractTestBase {
 
     @Mock
     private AmazonWebServicesClientProxy proxy;
@@ -38,30 +36,76 @@ public class ListHandlerTest extends AbstractTestBase {
 
     @BeforeEach
     public void setup() {
-        proxy = mock(AmazonWebServicesClientProxy.class);
-        handler = new ListHandler();
-        ecr = mock(EcrClient.class);
         proxyEcrClient = MOCK_PROXY(proxy, ecr);
+        handler = new ListHandler();
     }
 
     @Test
-    public void handleRequest_SimpleSuccess() {
-        // Setup
-        // Generate test request
+    void handleRequest_SimpleSuccess() {
+        doReturn(TestSdkResponseHelper.oneDestinationDescribeRegistryResponse())
+                .when(proxy)
+                .injectCredentialsAndInvokeV2(any(DescribeRegistryRequest.class), any());
+
         final ResourceHandlerRequest<ResourceModel> request =
-                TestHelper.generateRequest(TestHelper.singleDestination());
+                TestHelper.generateRequestWithPrimaryId(TestHelper.singleDestination());
 
         // Request
-        final ProgressEvent<ResourceModel, CallbackContext> response =
-                handler.handleRequest(proxy, request, new CallbackContext(), proxyEcrClient, logger);
+        final ProgressEvent<ResourceModel, CallbackContext> response
+                = handler.handleRequest(proxy, request, new CallbackContext(), proxyEcrClient, logger);
+
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(response).isNotNull();
+        softly.assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
+        softly.assertThat(response.getCallbackDelaySeconds()).isZero();
+        softly.assertThat(response.getResourceModel()).isNull();
+        softly.assertThat(response.getResourceModels()).isNotNull();
+        softly.assertThat(response.getResourceModels().get(0)).isEqualTo(request.getDesiredResourceState());
+        softly.assertAll();
+    }
+
+    @Test
+    void handleRequest_NoExistingResourceReturnsSuccessWithEmptyList() {
+        doReturn(TestSdkResponseHelper.emptyDescribeRegistryResponse())
+                .when(proxy)
+                .injectCredentialsAndInvokeV2(any(DescribeRegistryRequest.class), any());
+
+        final ResourceHandlerRequest<ResourceModel> request =
+                TestHelper.generateRequest(TestHelper.multipleDestinations());
+
+        // Request
+        final ProgressEvent<ResourceModel, CallbackContext> response
+                = handler.handleRequest(proxy, request, new CallbackContext(), proxyEcrClient, logger);
+
+        // Validation
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(response).isNotNull();
+        softly.assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
+        softly.assertThat(response.getCallbackDelaySeconds()).isZero();
+        softly.assertThat(response.getResourceModel()).isNull();
+        softly.assertThat(response.getResourceModels()).isEmpty();
+        softly.assertAll();
+    }
+
+    @Test
+    void handleRequest_ValidationExceptionReturnsFailedEvent() {
+        when(proxy.injectCredentialsAndInvokeV2(any(DescribeRegistryRequest.class), any()))
+                .thenThrow(ValidationException.builder().build());
+
+        final ResourceHandlerRequest<ResourceModel> request =
+                TestHelper.generateRequest(TestHelper.multipleDestinations());
 
 
-        assertThat(response).isNotNull();
-        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
-        assertThat(response.getCallbackDelaySeconds()).isZero();
-        assertThat(response.getResourceModels()).isNull();
-        assertThat(response.getMessage()).isEqualTo("List operation is not supported.");
-        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.InvalidRequest);
-        verifyNoInteractions(ecr);
+        // Request
+        final ProgressEvent<ResourceModel, CallbackContext> response
+                = handler.handleRequest(proxy, request, new CallbackContext(), proxyEcrClient, logger);
+
+        // Validation
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(response).isNotNull();
+        softly.assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        softly.assertThat(response.getResourceModel()).isNull();
+        softly.assertThat(response.getResourceModels()).isEmpty();
+        softly.assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.GeneralServiceException);
+        softly.assertAll();
     }
 }
